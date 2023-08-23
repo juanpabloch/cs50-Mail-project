15d7e92d928ae4600b179d0b2e91e5bb7d7b0648
JuanPablo <jchoternasty@the-8agency.com>
2023-08-23 13:16:41 -0300

    config database

--- project3/settings.py
+++ project3/settings.py
@@ -79,23 +79,18 @@ WSGI_APPLICATION = 'project3.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': os.getenv('DB_ENGINE'),
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT'),
-     }
     # 'default': {
-    #     'ENGINE': 'django.db.backends.postgresql',
-    #     'URL': os.getenv('POSTGRES_URL'),
-    #     'NAME': 'postgres',
-    #     'USER': os.getenv('POSTGRES_USER'),
-    #     'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
-    #     'HOST': os.getenv('POSTGRES_HOST'),
-    #     'PORT': '5432',
-    # }
+    #     'ENGINE': os.getenv('DB_ENGINE'),
+    #     'NAME': os.getenv('DB_NAME'),
+    #     'USER': os.getenv('DB_USER'),
+    #     'PASSWORD': os.getenv('DB_PASSWORD'),
+    #     'HOST': os.getenv('DB_HOST'),
+    #     'PORT': os.getenv('DB_PORT'),
+    #  }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 DATABASES['default'] = dj_database_url.config()
